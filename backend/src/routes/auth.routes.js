342ce3e7c1a021b3ece6dcffba97923396ddb8bd
DavidQuira <alejandroquira20@gmail.com>
2026-06-26 23:30:33 -0500

    middleware en token usuario

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const verificarToken = require('../middlewares/auth.middleware');
 
 const {
     register,
     login
 } = require('../controllers/auth.controller');
 
+router.get(
+    '/perfil',
+    verificarToken,
+    (req, res) => {
+
+        res.json({
+            message: 'Ruta protegida',
+            usuario: req.usuario
+        });
+
+    }
+);
     router.post('/register', register);
     router.post('/login', login);
 
