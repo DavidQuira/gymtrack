76e57cc6d951ad24a217e255d8c0634550700f39
DavidQuira <alejandroquira20@gmail.com>
2026-07-05 12:53:13 -0500

    get de perfil

--- backend/src/controllers/perfil.controller.js
+++ backend/src/controllers/perfil.controller.js
@@ -15,6 +15,26 @@ const crearPerfil = async (req, res) => {
             sexo
         } = req.body;
 
+
+
+
+        const perfilExistente = await pool.query(
+            `
+         SELECT id
+         FROM perfiles_usuario
+         WHERE usuario_id = $1
+         `,
+            [usuario_id]
+        );
+
+        if (perfilExistente.rows.length > 0) {
+            return res.status(409).json({
+                message: 'El usuario ya tiene un perfil creado'
+            });
+        }
+
+
+
         const nuevoPerfil = await pool.query(
             `
     INSERT INTO perfiles_usuario (usuario_id,
@@ -54,6 +74,44 @@ const crearPerfil = async (req, res) => {
 
 };
 
+
+const obtenerPerfil = async (req, res) => {
+
+    const usuario_id = req.usuario.id;
+
+    try {
+        const perfil = await pool.query(
+            `
+        SELECT *
+        FROM perfiles_usuario
+        WHERE usuario_id = $1
+        `,
+            [usuario_id]
+        );
+
+        if (perfil.rows.length === 0) {
+            return res.status(404).json({
+                message: 'No se encontró un perfil para este usuario'
+            });
+        }
+
+        return res.status(200).json({
+            message: 'Perfil obtenido correctamente',
+            perfil: perfil.rows[0]
+        });
+
+    } catch (error) {
+        console.error('Error al obtener el perfil:', error);
+        return res.status(500).json({
+            message: 'Error al obtener el perfil'
+        });
+    }
+
+
+}
+
+
 module.exports = {
-    crearPerfil
+    crearPerfil,
+    obtenerPerfil
 };
--- backend/src/routes/perfil.routes.js
+++ backend/src/routes/perfil.routes.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const router = express.Router();
-
 const verificarToken = require('../middlewares/auth.middleware');
 
 const {
-    crearPerfil
+    crearPerfil,
+    obtenerPerfil
 } = require('../controllers/perfil.controller');
+
 
 router.post(
     '/',
     verificarToken,
     crearPerfil
 );
+
+router.get(
+    '/',
+    verificarToken,
+    obtenerPerfil
+);
+
 
 module.exports = router;
